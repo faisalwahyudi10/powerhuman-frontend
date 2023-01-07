28b7ee1003be15a4a7c3ee88930359b5c1268519
Muhammad Faisal Wahyudi <faisal.wahyudi1390@gmail.com>
2023-01-08 06:35:50 +0800

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Hello Faisal</h1>
+</template>
+
+<script>
+export default {
+    name: 'AboutVue',
+    layout: 'blog',
+     middleware: ['auth'],
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Tutorial />
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'IndexPage',
+  async asyncData({ params }) {
+      const post = await axios.get(`https://api.nuxtjs.dev/posts/1`)
+      return { post }
+    },
+}
+</script>
